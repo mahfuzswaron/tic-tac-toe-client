9fcb3cfd41f9cce43b14e39839233eba5716d4dd
mahfuz swaron <mahfuzswaron@gmail.com>
2022-12-20 13:14:37 -0500

    gameCard done

--- src/app/components/GameCard.jsx
+++ src/app/components/GameCard.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import Button from './Button';
 
-const GameCard = () => {
+const GameCard = ({ game }) => {
+    const { players, status, date } = game;
     return (
-        <div>
-
+        <div className=' w-full p-4 rounded-lg shadow-[2px_4px_16px_0px_rgba(0,0,0,0.25)] space-y-4' >
+            <h1 className='text-2xl font-bold' >Game with {players[1]} </h1>
+            <p className='whitespace-pre-line ' >{status}</p>
+            <p className='text-sm'>{date} </p >
+            <Button type={"primary"}>Play!</Button>
         </div>
     );
 };
--- src/app/pages/Home.jsx
+++ src/app/pages/Home.jsx
@@ -1,15 +1,43 @@
 import React from 'react';
 import Button from '../components/Button';
+import GameCard from '../components/GameCard';
 import Heading1 from "../components/Heading1";
+
+const NoGameDiv = <div className=' flex-grow flex flex-col justify-center items-center my-auto max-h-min w-full'>
+    <h1 className='text-6xl text-center font-bilbo mb-6' >No Games Found</h1>
+    <Button type={"primary"} >Start a new game</Button>
+</div>
+
+const games = [
+    {
+        "_id": 1,
+        "players": ["mahfuz", "shahriya"],
+        "status": "you've made your move! \n Waiting for them",
+        "date": "20th December, 2022, 10:11pm"
+    },
+    {
+        "_id": 2,
+        "players": ["mahfuz", "shahriya"],
+        "status": "you've made your move! \n Waiting for them",
+        "date": "20th December, 2022, 10:11pm"
+    },
+]
 
 const Home = () => {
     return (
         <div className='w-full min-h-screen flex flex-col' >
             <Heading1>Your Games</Heading1>
-            <div className=' flex-grow flex flex-col justify-center items-center my-auto max-h-min w-full'>
-                <h1 className='text-6xl text-center font-bilbo mb-6' >No Games Found</h1>
-                <Button type={"primary"} >Start a new game</Button>
-
+            {/* {NoGameDiv} */}
+            <div className="grid grid-cols-1 gap-4 mt-4">
+                <GameCard game={games[0]} />
+                <GameCard game={games[0]} />
+                <GameCard game={games[0]} />
+                <GameCard game={games[0]} />
+                <GameCard game={games[0]} />
+                <GameCard game={games[0]} />
+                <GameCard game={games[0]} />
+                <GameCard game={games[0]} />
+                <GameCard game={games[1]} />
             </div>
         </div>
     );
