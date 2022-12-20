eee7b3cc1c485b852bafdd0a63b5564b69c2f980
mahfuz swaron <mahfuzswaron@gmail.com>
2022-12-20 12:27:41 -0500

    no games home page done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import Entry from "./app/pages/Entry";
 import Login from "./app/pages/Login";
 import Registar from "./app/pages/Registar";
+import Home from "./app/pages/Home";
 
 function App() {
   return (
     <div className="max-w-sm min-h-screen grid grid-cols-1 mx-auto p-4 border rounded-lg bg-white" >
       {/* <Entry /> */}
-      <Login />
+      {/* <Login /> */}
       {/* <Registar /> */}
+      <Home />
 
     </div>
   );
--- src/app/pages/Home.jsx
+++ src/app/pages/Home.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import Button from '../components/Button';
+import Heading1 from "../components/Heading1";
 
 const Home = () => {
     return (
-        <div>
+        <div className='w-full min-h-screen flex flex-col' >
+            <Heading1>Your Games</Heading1>
+            <div className=' flex-grow flex flex-col justify-center items-center my-auto max-h-min w-full'>
+                <h1 className='text-6xl text-center font-bilbo mb-6' >No Games Found</h1>
+                <Button type={"primary"} >Start a new game</Button>
 
+            </div>
         </div>
     );
 };
