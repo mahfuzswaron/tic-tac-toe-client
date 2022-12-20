8b62a8348923125747584373b726e2c3864a7946
mahfuz swaron <mahfuzswaron@gmail.com>
2022-12-20 07:56:25 -0500

    info component added

---
+++ src/app/components/Info.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Info = ({ children, type }) => {
+    return (
+        <div className={`px-[16px] py-[27px] rounded-lg text-white text-sm ${type === "success" ? "bg-success" : "bg-error"} `} >
+            {children}
+        </div>
+    );
+};
+
+export default Info;
--- src/app/pages/Registar.jsx
+++ src/app/pages/Registar.jsx
@@ -2,8 +2,9 @@ import React from 'react';
 import Heading2 from "../components/Heading2";
 import Heading1 from "../components/Heading1";
 import Button from "../components/Button";
+import Info from '../components/Info';
 
-const inputClasses = "mt-[12px] p-3 rounded-lg bg-gray w-full";
+const inputClasses = "mt-3 p-3 rounded-lg bg-gray w-full";
 
 const Registar = () => {
     return (
@@ -20,25 +21,30 @@ const Registar = () => {
                 <Heading1 additionalClasses="mt-[9px] " >Let’s get to know you better!</Heading1>
             </div>
 
-            <form>
+            <form >
                 {/* NAME  */}
-                <Heading2 additionalClasses={'mt-[16px]'} >Your name</Heading2>
+                <Heading2 additionalClasses={'mt-4'} >Your name</Heading2>
                 <input type={"text"} placeholder="Type your name here" className={inputClasses} />
 
                 {/* USERNAME  */}
-                <Heading2 additionalClasses={'mt-[16px]'} > username</Heading2>
+                <Heading2 additionalClasses={'mt-4'} > username</Heading2>
                 <input type={"text"} placeholder="Type your username here" className={inputClasses} />
 
                 {/* EMAIL  */}
-                <Heading2 additionalClasses={'mt-[16px]'} >Email</Heading2>
+                <Heading2 additionalClasses={'mt-4'} >Email</Heading2>
                 <input type={"email"} placeholder="Type your username here" className={inputClasses} />
 
                 {/* PASSWORD */}
-                <Heading2 additionalClasses={'mt-[16px]'} > Password</Heading2>
-                <input type={"text"} placeholder="Type your password here" className={inputClasses} />
+                <Heading2 additionalClasses={'mt-4'} > Password</Heading2>
+                <input type={"password"} placeholder="Type your password here" className={inputClasses} />
+
+                {/* white space  */}
+                <div className="mt-5 mb-[73px] "></div>
+
+                <Info type="success"> success </Info>
 
                 {/* SUBMIT BUTTON  */}
-                <Button additionalClasses="mt-[73px]" type="primary" >Register</Button>
+                <Button additionalClasses="mt-4" type="primary" >Register</Button>
             </form>
         </div>
     );
