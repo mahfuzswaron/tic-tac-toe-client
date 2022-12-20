f1e4d3ce1dcb73c93ef90759929c5f67b59967ed
mahfuz swaron <mahfuzswaron@gmail.com>
2022-12-20 13:45:03 -0500

    new game btn added

--- src/app/pages/Home.jsx
+++ src/app/pages/Home.jsx
@@ -25,7 +25,7 @@ const games = [
 
 const Home = () => {
     return (
-        <div className='w-full min-h-screen flex flex-col' >
+        <div className='w-full min-h-screen flex flex-col relative' >
             <Heading1>Your Games</Heading1>
             {/* {NoGameDiv} */}
             <div className="grid grid-cols-1 gap-4 mt-4">
@@ -38,6 +38,12 @@ const Home = () => {
                 <GameCard game={games[0]} />
                 <GameCard game={games[0]} />
                 <GameCard game={games[1]} />
+            </div>
+            <div className='w-[124px] p-2 flex items-center space-x-2 bg-[#270F36] text-white rounded-lg fixed bottom-6 right-6 z-1'>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="white" className="w-[14px] h-[14px]">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+                </svg>
+                <span className='text-[14px]' >New Game</span>
             </div>
         </div>
     );
