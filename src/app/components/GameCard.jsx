65ec740055a8de99b6080b85bde0d85f10ebaffc
mahfuz swaron <mahfuzswaron@gmail.com>
2023-01-05 07:54:17 +0600

    spacing issue fixed in gameCard

--- src/app/components/GameCard.jsx
+++ src/app/components/GameCard.jsx
@@ -18,11 +18,11 @@ const GameCard = ({ game, username }) => {
     const th = day === 1 ? "st" : day === 2 ? "nd" : day === 3 ? "rd" : "th"
     const dateString = `${day}${th} ${month} ${year}, ${hours12}:${minutes}${suffix}`
     return (
-        <div className=' w-full p-4 rounded-lg shadow-[2px_4px_16px_0px_rgba(0,0,0,0.25)] space-y-4' >
+        <div className=' w-full p-4 rounded-lg shadow-[2px_4px_16px_0px_rgba(0,0,0,0.25)] flex flex-col space-y-2' >
             <h1 className='text-2xl font-bold' >Game with {partner} </h1>
             <p className='whitespace-pre-line ' >{status.message[username]}</p>
             <p className='text-sm'>{dateString} </p >
-            <Link to={`/play-ground/${_id}`}><Button btnType={"primary"}>Play!</Button></Link>
+            <Link to={`/play-ground/${_id}`}><Button btnType={"primary"} >Play!</Button></Link>
         </div>
     );
 };
